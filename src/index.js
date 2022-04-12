272bc1f85d9ee583239199eb6a8c756818ab4cc7
nathansom <nathan.sa.official@gmail.com>
2022-04-12 01:38:59 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "https://odyssey-lift-off-server-w37s.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
